143b8a4c472bdf7c2b6160bdefc235e3fa6eb24e
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-07-05 09:49:06 +1200

    WIP

---
+++ docs/en/INDEX.md
@@ -0,0 +1,59 @@
+# tl;dr
+
+```php
+
+use Sunnysideup\ExportAllFromModelAdmin\ExportAllFromModelAdminTrait;
+class MyModelAdmin extends ModelAdmin
+{
+    use ExportAllFromModelAdminTrait;
+
+    //...
+}
+
+```
+
+If there are any fields that you would like to exclude from the export then you can
+add
+
+```php
+
+class MyModelToExport extends DataObject
+{
+    //... some example fields listed here...
+    private static $fields_to_exclude_in_export = [
+        //...
+        'Priority',
+        'MenuTitle',
+        'MetaTitle',
+        'MetaDescription',
+        'CanViewType',
+        'CanEditType',
+        'Version',
+        'ExtraMeta',
+        'ShowInMenus',
+        'ShowInSearch',
+        'Sort',
+        'HasBrokenFile',
+        'HasBrokenLink',
+        'ReportClass',
+        //...
+    ];
+    //...
+}
+
+```
+If there are any fields you would like to add:
+
+```php
+
+class MyModelToExport extends DataObject
+{
+    //... some example fields listed here...
+    private static $fields_to_include_in_export = [
+        //...
+        'MyDifficultRelation' => function($rel) {return $rel->getOtherStuff();},
+        'MyDate.Long' => 'Nice date for you',
+        //...
+    ];
+    //...
+}
---
+++ src/ExportAllFromModelAdminTrait.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Sunnysideup\ExportAllFromModelAdmin;
+
+use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+
+use SilverStripe\Core\Config\Config;
+
+use SilverStripe\Core\Injector\Injector;
+
+use SilverStripe\Assets\Image;
+
+use SilverStripe\Security\Member;
+
+
+trait ExportAllFromModelAdminTrait
+{
+
+    private static $separator = '|||';
+
+    private static $separator_replacer = '///';
+
+    public function getExportFields() : array
+    {
+        $className = $this->modelClass;
+        $singleton = Injector::inst()->get($className);
+        if($singleton) {
+            $exclude = Config::inst()->get($className, 'fields_to_exclude_from_export') ?:[];
+            $returnArray = [];
+            $fieldLabels = $singleton->FieldLabels();
+            $dbs = Config::inst()->get($className, 'db');
+            foreach(array_keys($dbs) as $fieldName) {
+                if(! in_array($fieldName, $exclude)) {
+                    $returnArray[$fieldName. '.Nice'] = $fieldLabels[$fieldName] ?? $fieldName;
+                }
+            }
+            $hasOne =
+                (Config::inst()->get($className, 'has_one') ? : []) +
+                (Config::inst()->get($className, 'belongs') ? : [])
+            ;
+            foreach($hasOne as $fieldName => $type) {
+                if(! in_array($fieldName, $exclude)) {
+                    switch($type) {
+                        case Image::class:
+                            $returnArray[$fieldName] = function($rel) {return $rel->Link();};
+                            break;
+                        case Member::class:
+                            $returnArray[$fieldName] = function($rel) {return $rel->Email;};
+                            break;
+                        default:
+                            $returnArray[$fieldName] = function($rel) {return $rel->getTitle();};
+                    }
+                }
+            }
+            $rels =
+                (Config::inst()->get($className, 'has_many') ? : []) +
+                (Config::inst()->get($className, 'many_many') ? : []) +
+                (Config::inst()->get($className, 'belongs_many_many') ? : [])
+            ;
+            $sep = Config::inst()->get(self::class, 'separator');
+            $sepReplacer = Config::inst()->get(self::class, 'separator_replacer');
+            foreach(array_keys($rels) as $fieldName) {
+                if(! in_array($fieldName, $exclude)) {
+                    $returnArray[$fieldName] = function($rels) {
+                        $a = [];
+                        foreach($rels as $rel) {
+                            $a[] = str_replace($sep, $sepReplacer, $rel->getTitle());
+                        }
+                        return implode(' '.$sep.' ', $a);
+                    };
+                }
+            }
+        } else {
+            $returnArray = parent::getExportFields();
+        }
+
+        return ksort($returnArray);
+    }
+}
