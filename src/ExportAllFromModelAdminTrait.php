a6939da37d72625b5549ac1343a9a604669f4f72
Nicolaas <github@sunnysideup.co.nz>
2022-07-14 14:52:23 +1200

    MAJOR!

--- src/ExportAllFromModelAdminTrait.php
+++ src/ExportAllFromModelAdminTrait.php
@@ -16,79 +16,120 @@ use SilverStripe\Control\Director;
 trait ExportAllFromModelAdminTrait
 {
 
-    private static $separator = '|||';
+    private static $fields_to_exclude_from_export_always = [
+        'BackLinks',
+    ];
 
-    private static $separator_replacer = '///';
+    private static $export_separator = '|||';
+
+    private static $export_separator_replacer = '///';
+
+    protected $exportFields = [];
+
+    protected $exportFieldLabels = [];
+    protected $exportFieldLabelsExclude = [];
 
     public function getExportFields() : array
     {
-        $className = $this->modelClass;
-        $singleton = Injector::inst()->get($className);
+
+        $singleton = Injector::inst()->get($this->modelClass);
         if($singleton) {
-            $exclude = Config::inst()->get($className, 'fields_to_exclude_from_export') ?:[];
-            $returnArray = [];
-            $fieldLabels = $singleton->FieldLabels();
-            $dbs = Config::inst()->get($className, 'db');
-            foreach(array_keys($dbs) as $fieldName) {
-                if(! in_array($fieldName, $exclude)) {
-                    $returnArray[$fieldName] = $fieldLabels[$fieldName] ?? $fieldName;
-                }
-            }
-            $casting = Config::inst()->get($className, 'casting');
-            foreach(array_keys($casting) as $fieldName) {
-                if(! in_array($fieldName, $exclude)) {
-                    $returnArray[$fieldName] = $fieldLabels[$fieldName] ?? $fieldName;
-                }
-            }
-            $hasOne =
-                (Config::inst()->get($className, 'has_one') ? : []) +
-                (Config::inst()->get($className, 'belongs') ? : [])
-            ;
-            foreach($hasOne as $fieldName => $type) {
-                if(! in_array($fieldName, $exclude)) {
-                    switch($type) {
-                        case Image::class:
-                            $returnArray[$fieldName] = function($rel) {return Director::absoluteURL($rel->Link());};
-                            break;
-                        case Member::class:
-                            $returnArray[$fieldName] = function($rel) {return $rel->Email;};
-                            break;
-                        default:
-                            $returnArray[$fieldName] = function($rel) {return $rel->getTitle();};
-                    }
-                }
-            }
-            $rels =
-                (Config::inst()->get($className, 'has_many') ? : []) +
-                (Config::inst()->get($className, 'many_many') ? : []) +
-                (Config::inst()->get($className, 'belongs_many_many') ? : [])
-            ;
-            foreach(array_keys($rels) as $fieldName) {
-                if(! in_array($fieldName, $exclude)) {
-                    $returnArray[$fieldName] = function($rels) {
-                        $sep = Config::inst()->get(self::class, 'separator');
-                        $sepReplacer = Config::inst()->get(self::class, 'separator_replacer');
-                        $a = [];
-                        foreach($rels as $rel) {
-                            $a[] = str_replace($sep, $sepReplacer, $rel->getTitle());
-                        }
-                        return implode(' '.$sep.' ', $a);
-                    };
-                }
-            }
+            $this->exportFieldLabelsExclude1 = Config::inst()->get($this->modelClass, 'fields_to_exclude_from_export') ?:[];
+            $this->exportFieldLabelsExclude2 = Config::inst()->get(self::class, 'fields_to_exclude_from_export_always') ? : self::$fields_to_exclude_from_export_always;
+            $this->exportFieldLabelsExclude = array_merge($this->exportFieldLabelsExclude1, $this->exportFieldLabelsExclude2);
+            $this->generateExportFieldLabels($singleton);
+            $this->exportFields = [];
+            $this->generateDbExportFields();
+            $this->generateCastingExportFields();
+            $this->generateHasOneExportFields();
+            $this->generateManyExportFields();
+
             if($singleton->hasMethod('getFieldsToIncludeInExport')) {
-                $returnArray += $singleton->getFieldsToIncludeInExport();
+                $this->exportFields += $singleton->getFieldsToIncludeInExport();
             }
             // if(Director::isDev()) {
-            //     foreach($returnArray as $fieldName => $title) {
+            //     foreach($this->exportFields as $fieldName => $title) {
             //         echo "\n'$fieldName',";
             //     }
             // }
         } else {
-            $returnArray = parent::getExportFields();
+            $this->exportFields = parent::getExportFields();
         }
-        ksort($returnArray);
+        ksort($this->exportFields);
 
-        return $returnArray;
+        return $this->exportFields;
+    }
+
+    protected function generateDbExportFields()
+    {
+
+        $dbs = Config::inst()->get($this->modelClass, 'db');
+        foreach(array_keys($dbs) as $fieldName) {
+            if(! in_array($fieldName, $this->exportFieldLabelsExclude)) {
+                $this->exportFields[$fieldName] = $this->exportFieldLabels[$fieldName] ?? $fieldName;
+            }
+        }
+    }
+
+    protected function generateCastingExportFields()
+    {
+        $casting = Config::inst()->get($this->modelClass, 'casting');
+        foreach(array_keys($casting) as $fieldName) {
+            if(! in_array($fieldName, $this->exportFieldLabelsExclude)) {
+                $this->exportFields[$fieldName] = $this->exportFieldLabels[$fieldName] ?? $fieldName;
+            }
+        }
+    }
+
+    protected function generateHasOneExportFields()
+    {
+        $hasOne =
+            (Config::inst()->get($this->modelClass, 'has_one') ? : []) +
+            (Config::inst()->get($this->modelClass, 'belongs') ? : [])
+        ;
+        foreach($hasOne as $fieldName => $type) {
+            if(! in_array($fieldName, $this->exportFieldLabelsExclude)) {
+                switch($type) {
+                    case Image::class:
+                        $this->exportFields[$fieldName] = function($rel) {return Director::absoluteURL($rel->Link());};
+                        break;
+                    case Member::class:
+                        $this->exportFields[$fieldName] = function($rel) {return $rel->Email;};
+                        break;
+                    default:
+                        $this->exportFields[$fieldName] = function($rel) {return $rel->getTitle();};
+                }
+            }
+        }
+    }
+
+    protected function generateManyExportFields()
+    {
+        $rels =
+            (Config::inst()->get($this->modelClass, 'has_many') ? : []) +
+            (Config::inst()->get($this->modelClass, 'many_many') ? : []) +
+            (Config::inst()->get($this->modelClass, 'belongs_many_many') ? : [])
+        ;
+        foreach(array_keys($rels) as $fieldName) {
+            if(! in_array($fieldName, $this->exportFieldLabelsExclude)) {
+                $this->exportFields[$fieldName] = function($rels) {
+                    $sep = Config::inst()->get(self::class, 'export_separator');
+                    $sepReplacer = Config::inst()->get(self::class, 'export_separator_replacer');
+                    $a = [];
+                    foreach($rels as $rel) {
+                        $a[] = str_replace($sep, $sepReplacer, $rel->getTitle());
+                    }
+                    return implode(' '.$sep.' ', $a);
+                };
+            }
+        }
+    }
+
+    protected function generateExportFieldLabels($singleton)
+    {
+        $singleton->FieldLabels();
+        foreach($this->exportFieldLabels as $key => $name) {
+            $this->exportFieldLabels[$key] = str_replace([',', '.'], '-', $name);
+        }
     }
 }
